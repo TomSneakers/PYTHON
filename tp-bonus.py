b770d3c4cd5706f28b742c29c57048b00bb9ad73
Tom <tomdesvignes031@gmail.com>
2024-01-08 14:54:00 +0100

    maj

--- tp-bonus.py
+++ tp-bonus.py
@@ -14,7 +14,7 @@ while True:
     elif tirage2 > tirage1:
         score_joueur2 += 1
 
-    # Chaque joueur tire 5 fois
+    # Chaque joueur tire 5 foi
     tirages1 = [random.randint(1, 6) for _ in range(5)]
     tirages2 = [random.randint(1, 6) for _ in range(5)]
 
